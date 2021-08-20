fad79cb00e27a11461eb61837235c6182daa8a7d
chrisdrifte <chrisdrifte@gmail.com>
2021-08-20 14:37:15 +0200

    Notes

--- src/main.ts
+++ src/main.ts
@@ -35,6 +35,37 @@ export const LunchMoneyCoinbaseConnection: LunchMoneyCryptoConnection<CoinbaseCo
   async getBalances(config, coinbase = defaultCoinbaseClient) {
     coinbase.setConfig(config);
 
+    /**
+     * We can get the following datapoints per cryptocurrency account:
+     *
+     * {
+     *  "id": "58542935-67b5-56e1-a3f9-42686e07fa40",
+     *  "name": "My Vault",
+     *  "primary": false,
+     *  "type": "vault",
+     *  "currency": "BTC",
+     *  "balance": {
+     *    "amount": "4.00000000",
+     *   "currency": "BTC"
+     *  },
+     *  "created_at": "2015-01-31T20:49:02Z",
+     *  "updated_at": "2015-01-31T20:49:02Z",
+     *  "resource": "account",
+     *  "resource_path": "/v2/accounts/58542935-67b5-56e1-a3f9-42686e07fa40",
+     *  "ready": true
+     * },
+     *
+     * The `crypto_balances` table can consume the following:
+     *
+     * Identifying fields: user_id, account_id, etc.
+     * Timestamp fields: timestamp (of when this balance was captured)
+     * Data fields: amount, currency, provider_currency, fiat_amount, fiat_currency
+     * Reference fields: crypto_account_id (to map to specific provider/API key)
+     * We will store historical balances in this table. Each row in this table represents a new daily balance. The latest balance on the account will be whatever is associated with the max timestamp. This logic is handled in Lunch Money core server.
+     * The most important thing here is being able to identify which set of balances represent the same account under the provider.
+     *
+     * @TODO update getAllBalances to return datapoints required by `crypto_balances`
+     */
     const balances = await coinbase.getAllBalances();
 
     return {
